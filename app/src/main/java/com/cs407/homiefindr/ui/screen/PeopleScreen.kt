de98d48cf8452db0e8bf57c7df053f65aaa423bf
Parker <brownemac4@icloud.com>
2025-10-28 14:10:08 -0500

    created navigation

--- app/src/main/java/com/cs407/homiefindr/ui/screen/PeopleScreen.kt
+++ app/src/main/java/com/cs407/homiefindr/ui/screen/PeopleScreen.kt
@@ -1,2 +1,9 @@
 package com.cs407.homiefindr.ui.screen
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun PeopleScreen() {
+    Text(text = "test")
+}
