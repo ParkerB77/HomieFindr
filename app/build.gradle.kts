1f6bfade44ed898d61199290e04dac9eb2b72614
AeAloysius <diao9@wisc.edu>
2025-11-20 15:54:46 -0600

    add myproile and chatting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,7 +57,7 @@ dependencies {
 
     // Firebase Auth (BoM controls versions)
     implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
-    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore-ktx")
 
 
